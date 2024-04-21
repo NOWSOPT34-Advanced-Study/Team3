a9ade9ec7a5225a752202fd177ac40ec2d65f9d4
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-04-22 07:52:05 +0900

    [feat] gradle sharedPreference | hilt | json 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -7,3 +7,15 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
+
+dependencies {
+    implementation(project(":domain"))
+    // sharedPreference
+    implementation(libs.core.ktx)
+    // crypto
+    implementation(libs.security.crypto)
+    // json
+    implementation(libs.kotlinx.serialization.json)
+    // dagger hilt
+    implementation(libs.dagger.hilt)
+}
