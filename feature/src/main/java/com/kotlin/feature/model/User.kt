2539639c5ce005872661e815a06f5ec7dedb708f
Eonji-sw <ejkim0625@gmail.com>
2024-04-21 21:49:11 +0900

    [MOVE] : feature 모듈로 feature 패키지 파일들 이동

---
+++ feature/src/main/java/com/kotlin/feature/model/User.kt
@@ -0,0 +1,20 @@
+package com.kotlin.feature.model
+
+import android.os.Parcelable
+import com.sopt.now.domain.entity.UserEntity
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class User(
+    val id: String,
+    val password: String,
+    val nickName: String,
+    val mbti: String
+) : Parcelable {
+    fun toUserEntity() = UserEntity(
+        id,
+        password,
+        nickName,
+        mbti
+    )
+}
