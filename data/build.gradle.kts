845aee88b97ebbbd9c1a0a189085ef86e9d1ea79
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-04-22 08:19:39 +0900

    [feat] gradle 수정 자바 -> 안드로이드

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,21 +1,40 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.jetbrainsKotlinJvm)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.dagger.hilt)
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+android {
+    namespace = "com.sopt.now.data"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
-    implementation(project(":domain"))
+//    implementation(project(":domain"))
     // sharedPreference
     implementation(libs.core.ktx)
-    // crypto
-    implementation(libs.security.crypto)
     // json
     implementation(libs.kotlinx.serialization.json)
     // dagger hilt
     implementation(libs.dagger.hilt)
+    kapt(libs.dagger.hilt.compiler)
 }
