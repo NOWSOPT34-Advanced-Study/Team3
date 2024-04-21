8150bdebdea3430a688bd464a43bb796f47d3813
Eonji-sw <ejkim0625@gmail.com>
2024-04-21 21:50:37 +0900

    [ADD] : feature module build.gradle add

---
+++ feature/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.dagger.hilt)
+}
+
+android {
+    namespace = "com.kotlin.feature"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+}
+
+dependencies {
+//    implementation(project(":core-ui"))
+//    implementation(project(":domain"))
+
+    // jetpack navi
+    implementation(libs.bundles.jetpack.navi)
+    // google
+    implementation(libs.material)
+    // ktx (by viewModels)
+    implementation(libs.activity.ktx)
+    implementation(libs.fragment.ktx)
+    // dagger hilt
+    implementation(libs.dagger.hilt)
+    kapt(libs.dagger.hilt.compiler)
+    // 기초 androidx 라이브러리 ("core-ktx", "constraintlayout", "appcompat", "activity")
+    implementation(libs.bundles.androidx)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.junit)
+    androidTestImplementation(libs.androidx.test.espresso)
+}
